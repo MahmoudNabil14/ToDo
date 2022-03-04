81c8bcf13391d4811a042cd61f949d2dca86c44a
Mahmoud Nabil <83550133+MahmoudNabil14@users.noreply.github.com>
2022-03-04 20:41:31 +0200

    Edit from home

--- lib/layout/main_layout/main_layout.dart
+++ lib/layout/main_layout/main_layout.dart
@@ -9,7 +9,6 @@ import 'package:flutter_gen/gen_l10n/app_localizations.dart';
 import 'package:fluttertoast/fluttertoast.dart';
 import 'package:intl/intl.dart';
 import '../../shared/state_manager/preferences_cubit/preferences_cubit.dart';
-import 'package:intl/date_symbol_data_local.dart';
 
 class MainLayout extends StatelessWidget {
   var scaffoldKey = GlobalKey<ScaffoldState>();
@@ -133,227 +132,229 @@ class MainLayout extends StatelessWidget {
           child: Drawer(
             child: Padding(
               padding: const EdgeInsets.all(20.0),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  DrawerHeader(
-                    child: Center(
-                      child: Text(
-                        'Tox'.toUpperCase(),
-                        style: TextStyle(
-                            fontFamily: 'Urial',
-                            fontSize: 60.0,
-                            color: Colors.blue),
+              child: SingleChildScrollView(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    DrawerHeader(
+                      child: Center(
+                        child: Text(
+                          'Tox'.toUpperCase(),
+                          style: TextStyle(
+                              fontFamily: 'Urial',
+                              fontSize: 60.0,
+                              color: Colors.blue),
+                        ),
                       ),
                     ),
-                  ),
-                  Text(
-                    AppLocalizations.of(context)!.drawerSettings,
-                    style: TextStyle(
-                        fontWeight: FontWeight.w800,
-                        fontSize: 22.0,
-                        color: Colors.blue),
-                  ),
-                  SizedBox(
-                    height: 5.0,
-                  ),
-                  Container(
-                    height: 1,
-                    color: Colors.grey[300],
-                    width: double.infinity,
-                  ),
-                  Row(
-                    children: [
-                      Icon(Icons.brightness_2),
-                      SizedBox(
-                        width: 8.0,
-                      ),
-                      Text(
-                        AppLocalizations.of(context)!.darkMode,
-                        style: TextStyle(
-                            fontSize: 18.0, fontWeight: FontWeight.bold),
-                      ),
-                      Switch(
-                          value:
-                              PreferencesCubit.get(context).darkModeSwitchIsOn,
-                          onChanged: (newValue) {
-                            PreferencesCubit.get(context).changeAppTheme();
-                          }),
-                    ],
-                  ),
-                  SizedBox(
-                    height: 10.0,
-                  ),
-                  Row(
-                    children: [
-                      Icon(Icons.language),
-                      SizedBox(
-                        width: 8.0,
-                      ),
-                      Text(
-                        AppLocalizations.of(context)!.language,
-                        style: TextStyle(
-                            fontSize: 18.0, fontWeight: FontWeight.bold),
-                      ),
-                      const SizedBox(
-                        width: 10.0,
-                      ),
-                      DropdownButton(
-                          value: PreferencesCubit.appLang,
-                          items: [
-                            'English',
-                            'العربية',
-                          ].map((e) {
-                            return DropdownMenuItem(
-                              child: Text(e),
-                              value: e,
-                            );
-                          }).toList(),
-                          onChanged: (value) {
-                            PreferencesCubit.get(context)
-                                .changeAppLanguage(value.toString(), context);
-                            print(PreferencesCubit.appLang);
-                            print(MainCubit.get(context).soundListValue);
-                          }),
-                    ],
-                  ),
-                  SizedBox(
-                    height: 10.0,
-                  ),
-                  Container(
-                    height: 1,
-                    color: Colors.grey[300],
-                    width: double.infinity,
-                  ),
-                  SizedBox(
-                    height: 5.0,
-                  ),
-                  Text(
-                    AppLocalizations.of(context)!.drawerHelpTitle,
-                    style: TextStyle(
-                        fontWeight: FontWeight.w800,
-                        fontSize: 22.0,
-                        color: Colors.blue),
-                  ),
-                  SizedBox(
-                    height: 5.0,
-                  ),
-                  Container(
-                    height: 1,
-                    color: Colors.grey[300],
-                    width: double.infinity,
-                  ),
-                  SizedBox(
-                    height: 8.0,
-                  ),
-                  InkWell(
-                    onTap: () {},
-                    child: Row(
+                    Text(
+                      AppLocalizations.of(context)!.drawerSettings,
+                      style: TextStyle(
+                          fontWeight: FontWeight.w800,
+                          fontSize: 22.0,
+                          color: Colors.blue),
+                    ),
+                    SizedBox(
+                      height: 5.0,
+                    ),
+                    Container(
+                      height: 1,
+                      color: Colors.grey[300],
+                      width: double.infinity,
+                    ),
+                    Row(
                       children: [
-                        Icon(Icons.help_outline_outlined),
+                        Icon(Icons.brightness_2),
                         SizedBox(
                           width: 8.0,
                         ),
                         Text(
-                          AppLocalizations.of(context)!.drawerHelpText,
+                          AppLocalizations.of(context)!.darkMode,
                           style: TextStyle(
                               fontSize: 18.0, fontWeight: FontWeight.bold),
                         ),
+                        Switch(
+                            value:
+                                PreferencesCubit.get(context).darkModeSwitchIsOn,
+                            onChanged: (newValue) {
+                              PreferencesCubit.get(context).changeAppTheme();
+                            }),
                       ],
                     ),
-                  ),
-                  SizedBox(
-                    height: 25.0,
-                  ),
-                  InkWell(
-                    onTap: () {},
-                    child: Row(
+                    SizedBox(
+                      height: 10.0,
+                    ),
+                    Row(
                       children: [
-                        Icon(Icons.share),
+                        Icon(Icons.language),
                         SizedBox(
                           width: 8.0,
                         ),
                         Text(
-                          AppLocalizations.of(context)!.drawerShareWithFriends,
+                          AppLocalizations.of(context)!.language,
                           style: TextStyle(
                               fontSize: 18.0, fontWeight: FontWeight.bold),
                         ),
+                        const SizedBox(
+                          width: 10.0,
+                        ),
+                        DropdownButton(
+                            value: PreferencesCubit.appLang,
+                            items: [
+                              'English',
+                              'العربية',
+                            ].map((e) {
+                              return DropdownMenuItem(
+                                child: Text(e),
+                                value: e,
+                              );
+                            }).toList(),
+                            onChanged: (value) {
+                              PreferencesCubit.get(context)
+                                  .changeAppLanguage(value.toString(), context);
+                              print(PreferencesCubit.appLang);
+                              print(MainCubit.get(context).soundListValue);
+                            }),
                       ],
                     ),
-                  ),
-                  SizedBox(
-                    height: 25.0,
-                  ),
-                  InkWell(
-                    onTap: () {},
-                    child: Row(
-                      children: [
-                        Icon(Icons.star_rate_outlined),
-                        SizedBox(
-                          width: 8.0,
-                        ),
-                        Text(
-                          AppLocalizations.of(context)!.drawerRate,
-                          style: TextStyle(
-                              fontSize: 18.0, fontWeight: FontWeight.bold),
-                        ),
-                      ],
+                    SizedBox(
+                      height: 10.0,
                     ),
-                  ),
-                  SizedBox(
-                    height: 10.0,
-                  ),
-                  Container(
-                    height: 1,
-                    color: Colors.grey[300],
-                    width: double.infinity,
-                  ),
-                  Align(
-                    alignment: Alignment.center,
-                    child: TextButton(
-                      onPressed: () {
-                        if (MainCubit.get(context).newTasks.isNotEmpty ||
-                            MainCubit.get(context).doneTasks.isNotEmpty ||
-                            MainCubit.get(context).archivedTasks.isNotEmpty) {
-                          MainCubit.get(context).deleteAllTask();
-                          NotificationManager.cancelAllNotification();
-                          Navigator.of(context).pop();
-                          Fluttertoast.showToast(
+                    Container(
+                      height: 1,
+                      color: Colors.grey[300],
+                      width: double.infinity,
+                    ),
+                    SizedBox(
+                      height: 5.0,
+                    ),
+                    Text(
+                      AppLocalizations.of(context)!.drawerHelpTitle,
+                      style: TextStyle(
+                          fontWeight: FontWeight.w800,
+                          fontSize: 22.0,
+                          color: Colors.blue),
+                    ),
+                    SizedBox(
+                      height: 5.0,
+                    ),
+                    Container(
+                      height: 1,
+                      color: Colors.grey[300],
+                      width: double.infinity,
+                    ),
+                    SizedBox(
+                      height: 8.0,
+                    ),
+                    InkWell(
+                      onTap: () {},
+                      child: Row(
+                        children: [
+                          Icon(Icons.help_outline_outlined),
+                          SizedBox(
+                            width: 8.0,
+                          ),
+                          Text(
+                            AppLocalizations.of(context)!.drawerHelpText,
+                            style: TextStyle(
+                                fontSize: 18.0, fontWeight: FontWeight.bold),
+                          ),
+                        ],
+                      ),
+                    ),
+                    SizedBox(
+                      height: 25.0,
+                    ),
+                    InkWell(
+                      onTap: () {},
+                      child: Row(
+                        children: [
+                          Icon(Icons.share),
+                          SizedBox(
+                            width: 8.0,
+                          ),
+                          Text(
+                            AppLocalizations.of(context)!.drawerShareWithFriends,
+                            style: TextStyle(
+                                fontSize: 18.0, fontWeight: FontWeight.bold),
+                          ),
+                        ],
+                      ),
+                    ),
+                    SizedBox(
+                      height: 25.0,
+                    ),
+                    InkWell(
+                      onTap: () {},
+                      child: Row(
+                        children: [
+                          Icon(Icons.star_rate_outlined),
+                          SizedBox(
+                            width: 8.0,
+                          ),
+                          Text(
+                            AppLocalizations.of(context)!.drawerRate,
+                            style: TextStyle(
+                                fontSize: 18.0, fontWeight: FontWeight.bold),
+                          ),
+                        ],
+                      ),
+                    ),
+                    SizedBox(
+                      height: 10.0,
+                    ),
+                    Container(
+                      height: 1,
+                      color: Colors.grey[300],
+                      width: double.infinity,
+                    ),
+                    Align(
+                      alignment: Alignment.center,
+                      child: TextButton(
+                        onPressed: () {
+                          if (MainCubit.get(context).newTasks.isNotEmpty ||
+                              MainCubit.get(context).doneTasks.isNotEmpty ||
+                              MainCubit.get(context).archivedTasks.isNotEmpty) {
+                            MainCubit.get(context).deleteAllTask();
+                            NotificationManager.cancelAllNotification();
+                            Navigator.of(context).pop();
+                            Fluttertoast.showToast(
+                                msg: AppLocalizations.of(context)!
+                                    .deleteTasksToast,
+                                toastLength: Toast.LENGTH_LONG,
+                                gravity: ToastGravity.BOTTOM,
+                                timeInSecForIosWeb: 1,
+                                backgroundColor: PreferencesCubit.get(context)
+                                        .darkModeSwitchIsOn
+                                    ? Colors.grey[700]
+                                    : Colors.grey[200],
+                                textColor: Colors.red,
+                                fontSize: 16.0);
+                          } else {
+                            Navigator.of(context).pop();
+                            Fluttertoast.showToast(
                               msg: AppLocalizations.of(context)!
-                                  .deleteTasksToast,
+                                  .deleteTasksToastFallback,
                               toastLength: Toast.LENGTH_LONG,
                               gravity: ToastGravity.BOTTOM,
                               timeInSecForIosWeb: 1,
-                              backgroundColor: PreferencesCubit.get(context)
-                                      .darkModeSwitchIsOn
-                                  ? Colors.grey[700]
-                                  : Colors.grey[200],
-                              textColor: Colors.red,
-                              fontSize: 16.0);
-                        } else {
-                          Navigator.of(context).pop();
-                          Fluttertoast.showToast(
-                            msg: AppLocalizations.of(context)!
-                                .deleteTasksToastFallback,
-                            toastLength: Toast.LENGTH_LONG,
-                            gravity: ToastGravity.BOTTOM,
-                            timeInSecForIosWeb: 1,
-                            backgroundColor: Colors.blue,
-                            textColor: Colors.white,
-                            fontSize: 16.0,
-                          );
-                        }
-                      },
-                      child: Text(
-                        AppLocalizations.of(context)!.drawerDeleteAllTasks,
-                        style: TextStyle(
-                            color: Colors.red,
-                            fontSize: 20.0,
-                            fontWeight: FontWeight.w900),
+                              backgroundColor: Colors.blue,
+                              textColor: Colors.white,
+                              fontSize: 16.0,
+                            );
+                          }
+                        },
+                        child: Text(
+                          AppLocalizations.of(context)!.drawerDeleteAllTasks,
+                          style: TextStyle(
+                              color: Colors.red,
+                              fontSize: 20.0,
+                              fontWeight: FontWeight.w900),
+                        ),
                       ),
-                    ),
-                  )
-                ],
+                    )
+                  ],
+                ),
               ),
             ),
           ),
